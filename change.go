260e39b1fd4825ddd400471cb0987d7785e12170
longsll <1506911471@qq.com>
2023-03-27 23:53:58 +0800

    change

--- change.go
+++ change.go
@@ -6,6 +6,20 @@ func change1(s byte) int { //字符'1'转数字1
 	return int(s) - 48
 }
 
+func change2(s byte) int { //字符'a'转数字1
+	return int(s) - 96
+}
+
+func change3(t int) byte { //数字1转字符'1'
+	return byte(t + 48)
+	//fmt.Printf("%c",change3(1))
+}
+
+func change4(t int) byte { //数字1转字符'a'
+	return byte(t + 96)
+	//fmt.Printf("%c",change4(1))
+}
+
 func main() {
-	fmt.Print(change1('1'))
+	fmt.Printf("%c",change4(1))
 }
