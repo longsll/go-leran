ee99012cf62ba9f6e5cf9b16415f1fe4c2b69024
longsll <1506911471@qq.com>
2023-04-12 22:50:31 +0800

    interface2

--- interface.go
+++ interface.go
@@ -42,4 +42,15 @@ func main () {
 	//computer调用iphone的usb接口
 	var comp = computer{}
 	comp.wokr(p1)
+
+	//空接口可以表示任何类型
+	var a interface{}
+	a = 20
+	fmt.Printf("%v , %T\n" , a , a)
+	a = "rap"
+	fmt.Printf("%v , %T\n" , a , a)
+	v , ok := a.(string) //类型断言
+		if ok { //判断成功
+			fmt.Println(v)
+		}
 }
